dd964245adfebad68a93252900aef4019c3f20c0
Yosef Sabag <yosefsabag46@gmail.com>
2025-10-16 22:44:31 +0300

    Added CORS configuration for Vercel

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,15 +1,23 @@
-// File: strapi/config/middlewares.js (ESM)
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000'],
+      enabled: true,
+      origin: [
+        'http://localhost:3000',
+        'https://onmotor-frontend.vercel.app',
+        'https://onmotor-frontend-f59b-rhq47hbv7-yosefs-projects-8bc687be.vercel.app',
+        'https://onmotormedia.com'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
     },
   },
+  'strapi::security',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
