318d4f0cf5ec81c848bbe1f1587197b2a7f65253
Yosef Sabag <yosefsabag46@gmail.com>
2025-10-28 16:40:58 +0200

    fix: convert configs to JS for Render + add Cloudinary baseUrl

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+      // ✅ תוספת קריטית להצגת תמונות תקינה בממשק Strapi
+      baseUrl: `https://res.cloudinary.com/${env('CLOUDINARY_NAME')}`,
+    },
+  },
+});
