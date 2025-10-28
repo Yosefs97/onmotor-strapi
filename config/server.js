b6ab06abf72ea1919bed22861543f248974b03ea
Yosef Sabag <yosefsabag46@gmail.com>
2025-10-28 21:01:31 +0200

    load

--- config/server.js
+++ config/server.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
-  url: env('PUBLIC_URL', 'https://onmotor-strapi.onrender.com'),
+  url: env('PUBLIC_URL'),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'EDUziIvM7y8Lm0qvu7Fl/w==',
+      'EedR0+7HvrjJFMwse//KpA==',
+      '+NUlITo6vvqLXHMOKzG6aQ==',
+      'kmS7Fnis9egocHnlcumbbw==',
+    ]),
   },
 });
