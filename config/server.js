0f043286524ca0df090b5006b561bc115f8c8e7d
Yosef Sabag <yosefsabag46@gmail.com>
2025-10-29 09:01:03 +0200

    load

--- config/server.js
+++ config/server.js
@@ -3,11 +3,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', [
-      'EDUziIvM7y8Lm0qvu7Fl/w==',
-      'EedR0+7HvrjJFMwse//KpA==',
-      '+NUlITo6vvqLXHMOKzG6aQ==',
-      'kmS7Fnis9egocHnlcumbbw==',
-    ]),
+    keys: env.array('APP_KEYS'),
   },
 });
