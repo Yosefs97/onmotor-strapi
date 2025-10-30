c5f5ae1106e5746504325a03e5cfaf2f337ba3fc
Yosef Sabag <yosefsabag46@gmail.com>
2025-10-30 13:07:03 +0200

    load

--- src/api/popular/controllers/popular.ts
+++ src/api/popular/controllers/popular.ts
@@ -1,19 +1,3 @@
-// src/api/popular/controllers/popular.ts
-import { factories } from '@strapi/strapi';
+import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::popular.popular', ({ strapi }) => ({
-  // פונקציה חדשה שמגדילה את מונה הצפיות
-  async incrementView(ctx) {
-    const { id } = ctx.params;
-
-    const entity = await strapi.db.query('api::popular.popular').findOne({ where: { id } });
-    if (!entity) return ctx.notFound('Popular item not found');
-
-    const updated = await strapi.db.query('api::popular.popular').update({
-      where: { id },
-      data: { views: (entity.views || 0) + 1 },
-    });
-
-    return ctx.send({ views: updated.views });
-  },
-}));
+export default factories.createCoreController('api::popular.popular');
--- src/api/popular/routes/popular.ts
+++ src/api/popular/routes/popular.ts
@@ -1,13 +1,7 @@
-// src/api/popular/routes/popular.ts
-export default {
-  routes: [
-    {
-      method: 'PUT',
-      path: '/populars/:id/increment-view',
-      handler: 'popular.incrementView',
-      config: {
-        auth:{ scope: ['public'] },
-      },
-    },
-  ],
-};
+/**
+ * popular router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::popular.popular');
