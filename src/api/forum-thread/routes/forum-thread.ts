94f265e11f9800c840ee8c47505b0951a66fecad
Yosef Sabag <yosefsabag46@gmail.com>
2025-11-11 17:02:43 +0200

    load

---
+++ src/api/forum-thread/routes/forum-thread.ts
@@ -0,0 +1,8 @@
+//src\api\forum-thread\routes\forum-thread.ts
+/**
+ * forum-thread router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::forum-thread.forum-thread');
